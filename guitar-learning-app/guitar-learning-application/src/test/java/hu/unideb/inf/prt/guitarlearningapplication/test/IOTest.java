3f31234e23dc042208ec9d847b901c65a142a4d1
David Kistamas <davidkistamas@gmail.com>
2016-06-03 21:10:05 +0200

    fix

--- guitar-learning-app/guitar-learning-application/src/test/java/hu/unideb/inf/prt/guitarlearningapplication/test/IOTest.java
+++ guitar-learning-app/guitar-learning-application/src/test/java/hu/unideb/inf/prt/guitarlearningapplication/test/IOTest.java
@@ -11,14 +11,12 @@ import org.junit.Test;
 import hu.unideb.inf.prt.guitarlearningapplication.Main;
 import hu.unideb.inf.prt.guitarlearningapplication.controller.ChordCreatorController;
 import hu.unideb.inf.prt.guitarlearningapplication.controller.IOController;
-import hu.unideb.inf.prt.guitarlearningapplication.model.Chord;
 
 public class IOTest {
 
 	private static ChordCreatorController chordCreatorController;
 	private static IOController IOcontroller;
 	private static Main main = new Main();
-	private Chord readyChord = null;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -46,21 +44,6 @@ public class IOTest {
 			IOcontroller.loadChordsFromFile(new File(getClass().getResource("/chords123.xml").toURI()));
 		} catch (URISyntaxException e) {
 			fail("Resource named chords123.xml cannot be opened!");
-		}
-	}
-	
-	@Test
-	public void testSaveChordToXmlFile() {
-				
-		readyChord = chordCreatorController.createChord(readyChord, "C", "MAJOR", 1, 3, 4, 3);
-		assertNotNull(readyChord);
-		
-		IOcontroller.saveChordAction(readyChord);
-		
-		try {
-			IOcontroller.loadChordsFromFile(new File(getClass().getResource("/chords.xml").toURI()));
-		} catch (URISyntaxException e) {
-			fail("Resource named chords.xml cannot be opened!");
 		}
 	}
 }
